80d32d76906b6c21fd9db402c5dde7ce04c137b2
Dipesh Poudel <43557135+Dpoudel18@users.noreply.github.com>
2025-01-17 10:13:25 -0500

    mlip lab1

--- analyze.py
+++ analyze.py
@@ -2,9 +2,10 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 import time
+import os
 
-endpoint = "ENTER ENDPOINT HERE"
-key = "ENTER KEY HERE"
+endpoint = os.getenv("ENDPOINT")
+key = os.getenv("KEY")
 
 credentials = CognitiveServicesCredentials(key)
 
